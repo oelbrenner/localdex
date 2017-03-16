4f4adb196f1e3540a0fb9f367078c53e866227db
oelbrenner <whiskeytangoeng@gmail.com>
2017-03-16 17:31:07 -0400

    scaffold lodgings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316141532) do
+ActiveRecord::Schema.define(version: 20170316212753) do
+
+  create_table "lodgings", force: :cascade do |t|
+    t.string   "destination_ref"
+    t.string   "region_ref"
+    t.string   "company_name"
+    t.string   "address_line_one"
+    t.string   "address_line_two"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.string   "phone"
+    t.string   "reservation"
+    t.string   "fax"
+    t.string   "url"
+    t.string   "contact"
+    t.string   "season"
+    t.string   "email"
+    t.string   "details"
+    t.string   "mask2id_ref"
+    t.string   "customer_status_notes"
+    t.datetime "expires_at"
+    t.datetime "paid_at"
+    t.integer  "user_id"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.index ["user_id"], name: "index_lodgings_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
