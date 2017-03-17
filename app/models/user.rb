5a09b4ba5681eab51da548fe51c3f3c0ae8b108a
oelbrenner <whiskeytangoeng@gmail.com>
2017-03-17 16:12:53 -0400

    wire up user lodging association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   devise :invitable, :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  has_many :lodgings
+  accepts_nested_attributes_for :lodgings
 end
