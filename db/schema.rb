ea96909b1f6a4f0f2c7193dbee2a5a5fa629c23c
oelbrenner <whiskeytangoeng@gmail.com>
2017-03-20 18:38:03 -0400

    update local database to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170316212753) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "lodgings", force: :cascade do |t|
     t.string   "destination_ref"
     t.string   "region_ref"
@@ -36,7 +39,7 @@ ActiveRecord::Schema.define(version: 20170316212753) do
     t.integer  "user_id"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
-    t.index ["user_id"], name: "index_lodgings_on_user_id"
+    t.index ["user_id"], name: "index_lodgings_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -60,11 +63,11 @@ ActiveRecord::Schema.define(version: 20170316212753) do
     t.string   "invited_by_type"
     t.integer  "invited_by_id"
     t.integer  "invitations_count",      default: 0
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
-    t.index ["invitations_count"], name: "index_users_on_invitations_count"
-    t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
+    t.index ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
+    t.index ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
