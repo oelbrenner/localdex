646c4294027994fb70d374283800a4871dd953e9
oelbrenner <whiskeytangoeng@gmail.com>
2017-04-11 20:39:40 -0400

    fixes and tweaks to e-postcard html rendering

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,6 @@ class PagesController < ApplicationController
   before_action :set_page, only: [:show, :edit, :update, :destroy]
   layout 'page'
 
-  def new
-  end
 
   def show
     @epostcards = @page.epostcards.where.not(id:  nil)
@@ -15,19 +13,23 @@ class PagesController < ApplicationController
     @new_epostcard = Epostcard.new
   end
 
-  def create
-    @page = Page.new(page_params)
+  def new
+    @lodging = Lodging.find(params[:lodging_id])
+    @page = Page.new(content_header: 'Send a e-postcard!', content_one: "#{@lodging.company_name} is happy to bring you these custom e-postcards", content_two: 'Select one below, and send a note back home', page_type: "e-postcard", active: true, lodging_id: @lodging.id)
     @new_epostcard = Epostcard.new
 
     respond_to do |format|
       if @page.save
-        format.html { redirect_to @page, notice: 'Page was successfully created.' }
+        format.html { redirect_to edit_page_path(@page), notice: 'Page was successfully created.' }
         format.json { render :show, status: :created, location: @page }
       else
         format.html { render :new }
         format.json { render json: @page.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  def create
   end
 
   def update
@@ -55,6 +57,7 @@ class PagesController < ApplicationController
     def set_page
       @page = Page.find(params[:id])
     end
+
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def page_params
